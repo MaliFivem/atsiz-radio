92e05908d9b325b56d018c4437d0033804fb8aec
Enes Alper K <alperkrgz035@gmail.com>
2020-05-10 18:48:52 +0300

    Update items.sql

--- atsiz-radio/items.sql
+++ atsiz-radio/items.sql
@@ -3,5 +3,5 @@ INSERT INTO `items` (`name`, `label`, `rare`, `can_remove`) VALUES
 	('bozuktelefon', 'Bozuk Telefon', 0, 1);
 	('telsiztamir', 'Telsiz Tamir Kiti', 0, 1),
 	('bozuktelsiz', 'Bozuk Telsiz', 0, 1),
-	('battery', 'Pil', 0, 1),
+	('battery', 'Pil', 0, 1);
 
